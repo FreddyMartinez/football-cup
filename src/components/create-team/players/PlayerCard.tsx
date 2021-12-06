681bc3471a8c485cff2c0d80486b8c0f318415b2
FreddyMartinez <fhmartinezq@gmail.com>
2021-12-06 01:33:24 +0100

    player-card component was created

--- src/components/create-team/players/PlayerCard.tsx
+++ src/components/create-team/players/PlayerCard.tsx
@@ -1,13 +1,32 @@
-import { PlayerInterface } from "../../../util/playerInterface";
+import { useContext } from "react";
+import TeamContext from "../../../context/teams/TeamContext";
+import { CoachRole } from "../../../util/constants";
+import { PlayerInterface } from "../../../util/interfaces";
 import "./PlayerCard.css";
 
 const PlayerCard = ({ player }: { player: PlayerInterface }) => {
+  const teamContext = useContext(TeamContext)
+  const { addPlayer, setCoach } = teamContext;
+
+  const addPlayerOnClick = () => {
+    if (player.role === CoachRole) {
+      setCoach && setCoach(player)
+    } else {
+      addPlayer && addPlayer(player)
+    }
+  }
+
   return (
     <div className='card player-card'>
+      <img src='./player.jpg' alt='' />
       <span>{player.name}</span>
-      <span>Country: {player.country}</span>
-      <span>Position: {player.position}</span>
-      <button className="btn">Add to my team</button>
+      <span>Country: {player.nationality}</span>
+      {player.role === CoachRole ? (
+        <span>Coach</span>
+      ) : (
+        <span>Position: {player.position}</span>
+      )}
+      <button className='btn add-btn' onClick={addPlayerOnClick}>Add to my team</button>
     </div>
   );
 };
